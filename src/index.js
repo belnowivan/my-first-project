e3e55d662fe0bd4be499cd3fae3d47fc37dd6270
IVBEL <belnow.ivan@yandex.ru>
2018-05-09 16:28:30 +0300

    add 'export default'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const welcomeUser = () => {
-  console.log("Welcome to the Brain Games!");
+const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have you name? ');
   console.log(`Hello, ${userName}!`);
 };
+export default welcomeUser;
