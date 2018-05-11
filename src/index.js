b721de3d0bb56e9430d0559e79923b234f827309
IVBEL <belnow.ivan@yandex.ru>
2018-05-11 17:28:10 +0300

    fix format

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const welcomeUser = () => {
-  console.log('Welcome to the Brain Games!');
+export const UserName = () => {
   const userName = readlineSync.question('May I have you name? ');
   console.log(`Hello, ${userName}!`);
 };
-export default welcomeUser;
+export const mainGames = () => {
+  console.log('Welcome to the Brain Games!\n');
+  UserName();
+};
+export const welcomeGameEven = () => console.log('Welcome to Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
