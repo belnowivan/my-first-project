af13f9f814d5f5ba3ae6fe7bf7d4e5bcf1424d9b
IVBEL <belnow.ivan@yandex.ru>
2018-05-12 15:40:47 +0300

    rm local const 'endgame', add global const 'game step'

--- src/games/even_games.js
+++ src/games/even_games.js
@@ -3,9 +3,9 @@ import { startGame, welcomeGameEven } from '..';
 
 export const numberRandom = () => Math.floor((Math.random() * 100) + 1);
 const isEven = number => number % 2 === 0;
+export const gameStep = 3;
 
 const gameEvenProcces = (userName, number = numberRandom(), iter = 1) => {
-  const endGame = 3;
   console.log(`Question: ${number}`);
   const userAnswer = readlineSync.question('Your answer: ');
   const trueAnswer = isEven(number) ? 'yes' : 'no';
@@ -15,7 +15,7 @@ const gameEvenProcces = (userName, number = numberRandom(), iter = 1) => {
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again, ${userName}`);
     return;
   }
-  if (iter === endGame) {
+  if (iter === gameStep) {
     console.log(`Congratulations, ${userName}!`);
     return;
   }
